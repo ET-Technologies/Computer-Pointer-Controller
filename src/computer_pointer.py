df3d21b0309133df88df8488defeeba92c930d8a
ET Tech <erwin@entrich-technologies.com>
2020-08-20 12:11:35 +0200

    Create computer_pointer.py

---
+++ src/computer_pointer.py
@@ -0,0 +1,11 @@
+import time
+import face_detection as fd
+import facial_landmarks_detection as fl
+import head_pose_estimation ad hp
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    main()
